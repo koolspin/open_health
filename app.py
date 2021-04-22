0f3df554bde351446c2641076f887eb618995121
colin-crestron <cturner@crestron.com>
2021-04-21 22:27:14 -0400

    More code - right from the tutorial!

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import os
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'open_health.db'),
+    )
+
+from . import auth
+app.register_blueprint(auth.bp)
+from . import db
+db.init_app(app)
 
 
 @app.route('/')
---
+++ auth.py
@@ -0,0 +1,37 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+#from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # db = get_db()
+        error = None
+        # user = db.execute(
+        #     'SELECT * FROM user WHERE username = ?', (username,)
+        # ).fetchone()
+
+        user = 'Colin'
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
